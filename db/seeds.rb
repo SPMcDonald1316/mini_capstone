5bdfd907431cc709d09a2e71b5c6b64c7b8f62d1
Sean McDonald <spmcdonald1316@gmail.com>
2020-03-04 12:40:29 -0600

    create products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(name: "Marucci AP5 Albert Pujols Custom Pro Maple Bat", price: 169, image_url: "https://dks.scene7.com/is/image/GolfGalaxy/19MCCACSTMP5PRMDLWBT_Custom?wid=685&fmt=jpg", description: "Inspired by the core design used by one of the game’s greatest sluggers, the Marucci® AP5 Albert Pujols Custom Pro Maple Bat lays down the foundation for powerful performance at the plate while you personalize a number of key details for a completely unique experience. This bat is bone-rubbed to deliver a denser, more durable design, and offers an end-loaded feel to bolster the impact of your swing to send the ball sailing.")
+
+product.save
