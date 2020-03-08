377109fea7bc4d90182e11270b37300657e00717
Sean McDonald <spmcdonald1316@gmail.com>
2020-03-08 11:27:58 -0500

    rewrite update method

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -22,10 +22,12 @@ class Api::ProductsController < ApplicationController
 
   def update
     @product = Product.find_by(id: params[:id])
-    @product.name = params[:name] || @product.name
-    @product.price = params[:price] || @product.price
-    @product.image_url = params[:image_url] || @product.image_url
-    @product.description = params[:description] || @product.description
+    @product.update(
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      image_url: params[:image_url] || @product.image_url,
+      description: params[:description] || @product.description
+    )
     @product.save
     render 'show.json.jb'
   end
