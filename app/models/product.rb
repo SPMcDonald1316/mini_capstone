a8b53cfa19e0b81f556113a71e0d3d43b7319b77
Sean McDonald <spmcdonald1316@gmail.com>
2020-03-16 20:44:58 -0500

    link tables and change json to render supplier name and product images

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,9 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: {greater_than: 0}
   validates :description, length: {in: 10..500}
 
+  belongs_to :supplier
   has_many :images
-  
+
   def is_discounted?
     price < 100
   end
@@ -16,9 +17,5 @@ class Product < ApplicationRecord
 
   def total
     price + tax
-  end
-
-  def supplier
-    Supplier.find_by(id: supplier_id)
   end
 end
